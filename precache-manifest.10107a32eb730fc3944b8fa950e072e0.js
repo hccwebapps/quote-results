8cdf186bcc6a357e831c4d460ac3218d44d1471a
tobyl <tobyleftly@gmail.com>
2020-06-03 15:30:03 -0400

    Updates

---
+++ precache-manifest.10107a32eb730fc3944b8fa950e072e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "283854934e3acba68679fca83271f409",
+    "url": "/quote-results/index.html"
+  },
+  {
+    "revision": "1968096ac15131166342",
+    "url": "/quote-results/static/css/main.531eb21c.chunk.css"
+  },
+  {
+    "revision": "6112bb1a4599f8fd628c",
+    "url": "/quote-results/static/js/2.992de4d4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/quote-results/static/js/2.992de4d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1968096ac15131166342",
+    "url": "/quote-results/static/js/main.7bcda519.chunk.js"
+  },
+  {
+    "revision": "6244cea6a8cdd76217e6",
+    "url": "/quote-results/static/js/runtime-main.6e23a156.js"
+  }
+]);
